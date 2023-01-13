27b6f9aa87b6ef6ac019fd8137946bc5a3518e93
Bdioui Pierre <bdiouipierre@gmail.com>
2023-01-13 11:38:49 +0400

    update-prod

--- report/views.py
+++ report/views.py
@@ -14,6 +14,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     if request.method == 'POST':
         feder_form = CSV_load(request.POST, request.FILES)
